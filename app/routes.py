c1770db9e008abc78fffbe0b6b64588ead90426f
I530641 <wallace.lim@sap.com>
2020-01-10 13:16:59 +0800

    Add follower/following relationship between users + related functionalities

--- app/routes.py
+++ app/routes.py
@@ -81,3 +81,36 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    curent_user.follow(user)
+    db.session.commit()
+    flash('You are now following {}.'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfolow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    curent_user.unfollow(user)
+    db.session.commit()
+    flash('You are no longer following {}.'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+
